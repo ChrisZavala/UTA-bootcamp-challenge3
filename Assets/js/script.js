e65fb4a6081b2e8796efbf9053982236f0b6521c
Chris Zavala <frappejcat@gmail.com>
2022-12-21 22:31:48 -0600

    still adding js to get a little further

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,18 +1,4 @@
-//must return a string value that is the password. 
-  // prompt user to choose num between 8 and 128
-    //validate user num
-    //confirm prompt do you want to include lowercase, uppercase, numeric and/or special chars (4 confirm prompts)
-    // 4 vars to save T/F to included character
-    // Vars that include all user options of each category. Array of strings for eat category. 
-    // var upper = ["A", "B"]
-    
-    //if my user yes to include category choose random(Math.random) elements from the array
-    //how to decide how many elements to choose from each array
-    // validate that at least one category was chosen
-    //join chosen elements into a string 
-    //at least one for loop
-    //lots of IF statements. 
-    // Assignment Code
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
@@ -23,16 +9,30 @@ var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"
 var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharArray = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "+", "_", "=",];
-
+//how to decide how many elements to choose from each array
 var resultArray = [];
 
 var userArray = [];
 
-  
-  return passwordString;
+//confirm prompt do you want to include lowercase, uppercase, numeric and/or special chars (4 confirm prompts)
+//Here I am going to put all my prompts to ask the user for the conditions fo the password generation. 
+
+upperCaseArray [1];
+
+// prompt user to choose num between 8 and 128
+//validate user num
+
+var numberChar = window.prompt("Choose a length of at least 8 characters and no more than 128");
+
+// 4 vars to save T/F to included character
+// validate that at least one category was chosen
+var lowerCase = window.confirm("You confirm you want Lowercase letters in your password?");
+var upperCase = window.confirm("You confirm you want Uppercase letters in your password?");
+var numeric = window.confirm("You confirm you want numbers in your password");
+var specialChar = window.confirm("You confirm you want special characters in your password?");
 
 
-}
+
 
 
 // Write password to the #password input
@@ -43,8 +43,7 @@ function writePassword() {
   passwordText.value = password;
   
 }
-
-
+console.log(password);
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
